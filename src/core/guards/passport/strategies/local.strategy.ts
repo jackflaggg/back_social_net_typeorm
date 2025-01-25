5d3a205d887ccffda922e095367e12647a2f188e
jackflaggg <rasul.khamzinnn@gmail.com>
2025-01-25 18:24:17 +0500

    add passport

--- src/core/guards/passport/strategies/local.strategy.ts
+++ src/core/guards/passport/strategies/local.strategy.ts
@@ -1,15 +1,22 @@
+import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { Strategy } from 'passport';
+import { UserDocument } from 'src/features/user-accounts/domain/user/user.entity';
+import { UnauthorizedDomainException } from '../../../exceptions/incubator-exceptions/domain-exceptions';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
-    constructor(private readonly authService: AuthService) {
-        super({
-            usernameField: 'email',
-        });
+export class LocalStrategy extends PassportStrategy(Strategy) {
+    constructor(private authService: any) {
+        //поле в теле запроса, которое будет использоваться для валидации
+        super({ usernameField: 'loginOrEmail' });
     }
-    validate(email: string, password: string) {
-        return this.authService.validateUser(email, password);
+
+    async validate(username: string, password: string): Promise<UserDocument> {
+        const user = await this.authService.validateUser({ loginOrEmail: username, password });
+        if (!user) {
+            throw UnauthorizedDomainException.create();
+        }
+
+        return user;
     }
 }
--- src/core/guards/passport/strategies/refresh.strategy.ts
+++ src/core/guards/passport/strategies/refresh.strategy.ts
@@ -1,30 +1,41 @@
-import { PassportStrategy } from '@nestjs/passport';
 import { Inject, Injectable } from '@nestjs/common';
-import { Strategy } from 'passport';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { UserRepository } from '../../../../features/user-accounts/infrastructure/user.repository';
+import { ConfigService } from '@nestjs/config';
+import { UnauthorizedDomainException } from '../../../exceptions/incubator-exceptions/domain-exceptions';
 
 @Injectable()
-export class RefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+export class JwtRefreshAuthPassportStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
     constructor(
-        @Inject(refreshConfig.KEY) private readonly refreshConfiguration: ConfigType<typeof refreshConfig>,
-        private readonly authService: AuthService,
+        @Inject() private readonly usersRepository: UserRepository,
+        @Inject() private readonly securityDevicesRepository: any,
+        public readonly coreConfig: ConfigService,
     ) {
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                req => {
+                    // Extract the JWT from the cookie
+                    return req.cookies?.refreshToken; // Ensure that the cookie name matches what you set
+                },
+            ]),
             ignoreExpiration: false,
-            secretOrKey: refreshConfiguration.secret,
-            // для того, чтоб передать реквест
-            passReqToCallback: true,
+            secretOrKey: coreConfig.getOrThrow('refreshTokenSecret'),
         });
     }
-    /**
-     * функция принимает payload из токена и возвращает то, что будет записано в req.user
-     * @param payload
-     */
-    async validate(req: Request, payload: AuthJWTPayload) {
-        // в пэйлоуд будут лежать раскодированные данные!!!!!
-        console.log(payload, req.headers.authorization);
-        const refreshToken = req.get('authorization').replace('Bearer', '').trim();
-        const userId = payload.sub;
-        return await this.authService.validateRefreshToken(userId, refreshToken);
+
+    async validate(payload: any) {
+        const user = await this.usersRepository.findUserByIdOrFail(payload.userId);
+        if (!user) {
+            throw UnauthorizedDomainException.create();
+        }
+
+        const device = await this.securityDevicesRepository.findDeviceByToken(payload);
+
+        if (!device) {
+            throw UnauthorizedDomainException.create();
+        }
+
+        return payload;
     }
 }
