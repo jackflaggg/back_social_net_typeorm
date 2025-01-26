c48f8961efc719ea0f642226133a44e19c3c1992
jackflaggg <rasul.khamzinnn@gmail.com>
2025-01-26 19:26:23 +0500

    add decorators

--- libs/contracts/commands/auth/login.command.ts
+++ libs/contracts/commands/auth/login.command.ts
@@ -1,11 +1,31 @@
 import { z } from 'zod';
+import { passwordConstraints } from '../../constants/auth/auth-property.constraints';
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const loginRegex = /^[a-zA-Z0-9.-]+$/;
 
 const UserLoginRequestSchema = z.object({
     //TODO#1: Сделать в зависимости от введенных данных!
-    loginOrEmail: z.string(),
-    password: z.string(),
+    loginOrEmail: z.string().refine((value: any) => {
+        if (typeof value !== 'string' || value.trim() === '') {
+            return false;
+        }
+
+        const trimmedValue = value.trim();
+
+        if (trimmedValue.length < 3) {
+            return false;
+        }
+
+        if (trimmedValue.includes('@')) {
+            return emailRegex.test(trimmedValue);
+        }
+
+        return loginRegex.test(trimmedValue);
+    }),
+    password: z.string().min(passwordConstraints.minLength).max(passwordConstraints.maxLength).trim(),
     ip: z.string(),
-    agent: z.string(),
+    userAgent: z.string(),
 });
 
 const UserLoginResponseSchema = z.object({
