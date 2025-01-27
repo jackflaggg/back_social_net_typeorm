b2a50dca12b4419e6702a560f34cd81daf708962
jackflaggg <rasul.khamzinnn@gmail.com>
2025-01-27 19:50:59 +0500

    add jwtservice

--- src/features/user-accounts/infrastructure/sessions/query/session.query.repository.ts
+++ src/features/user-accounts/infrastructure/sessions/query/session.query.repository.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { UserViewDto } from '../../../dto/api/user-view.dto';
 import { DeviceEntity, DeviceModelType } from '../../../domain/device/device.entity';
 import { DeviceViewDto } from '../../../dto/repository/device-view.dto';
 
@@ -8,7 +7,7 @@ import { DeviceViewDto } from '../../../dto/repository/device-view.dto';
 export class SessionQueryRepository {
     constructor(@InjectModel(DeviceEntity.name) private deviceModel: DeviceModelType) {}
 
-    async getSessions(userId: string): Promise<DeviceViewDto | void> {
+    async getSessions(userId: string): Promise<DeviceViewDto[] | void> {
         const session = await this.deviceModel.find({ userId });
         return session ? session.map(elem => DeviceViewDto.mapToView(elem)) : [];
     }
