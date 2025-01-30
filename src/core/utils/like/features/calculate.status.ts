d28e64799606bfc0d8a24505ca235836ff1ce1f6
jackflaggg <rasul.khamzinnn@gmail.com>
2025-01-30 20:08:22 +0500

    add calculateStatus

---
+++ src/core/utils/like/features/calculate.status.ts
@@ -0,0 +1,48 @@
+import { StatusLike } from '@libs/contracts/enums/status.like';
+
+export function calculateStatus(currentStatus: string, changedStatus: string) {
+    let likesCount = 0;
+    let dislikesCount = 0;
+
+    if (currentStatus === StatusLike.enum['Like'] && changedStatus === StatusLike.enum['Dislike']) {
+        likesCount = -1;
+        dislikesCount = 1;
+    }
+
+    if (currentStatus === StatusLike.enum['Like'] && changedStatus === StatusLike.enum['None']) {
+        likesCount = -1;
+        dislikesCount = 0;
+    }
+
+    if (currentStatus === StatusLike.enum['Dislike'] && changedStatus === StatusLike.enum['None']) {
+        likesCount = 0;
+        dislikesCount = -1;
+    }
+
+    if (currentStatus === StatusLike.enum['Dislike'] && changedStatus === StatusLike.enum['Like']) {
+        likesCount = 1;
+        dislikesCount = -1;
+    }
+
+    if (currentStatus === StatusLike.enum['None'] && changedStatus === StatusLike.enum['Like']) {
+        likesCount = 1;
+        dislikesCount = 0;
+    }
+
+    if (currentStatus === StatusLike.enum['None'] && changedStatus === StatusLike.enum['Dislike']) {
+        likesCount = 0;
+        dislikesCount = -1;
+    }
+
+    // if (currentStatus === commentStatus.DISLIKE && changedStatus === commentStatus.DISLIKE) {
+    //     likesCount = 0
+    //     dislikesCount = -1
+    // }
+    //
+    // if (currentStatus === commentStatus.LIKE && changedStatus === commentStatus.LIKE) {
+    //     likesCount = -1
+    //     dislikesCount = 0
+    // }
+
+    return { likesCount, dislikesCount };
+}
