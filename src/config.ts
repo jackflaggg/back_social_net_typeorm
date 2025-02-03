e1ff766cfed746e906d159be15c46e8430a574d0
jackflaggg <rasul.khamzinnn@gmail.com>
2025-02-03 23:09:32 +0500

    add env

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,12 @@
 import { ConfigModule } from '@nestjs/config';
+import process from 'node:process';
 
 export const configModule = ConfigModule.forRoot({
     envFilePath: [
         // у первого файлика приоритет выше!
-        '.env.production'.trim() || '.env.development'.trim(),
+        `.env.${process.env.NODE_ENV}.local`,
+        `.env.${process.env.NODE_ENV}`,
+        '.env.production',
         '.env.development',
     ],
 });
