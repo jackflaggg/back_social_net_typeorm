0ee6770e9730f8e4e5f5a86ae0c823c433ff67f4
jackflaggg <rasul.khamzinnn@gmail.com>
2025-02-04 17:37:35 +0500

    add core config

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,20 @@ import { JwtModule } from '@nestjs/jwt';
 import { configModule } from './config';
 import { CoreConfig } from './core/config/core.config';
 import { CoreModule } from './core/config/config.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
         // всегда нужно задавать выше всех модулей, иначе другие модули не увидят енв!
         CoreModule,
         configModule,
-        JwtModule.register({
-            secret: 'not_envelope',
-            signOptions: { expiresIn: '5m' },
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [CoreConfig],
+            useFactory: async (coreConfig: CoreConfig) => ({
+                secret: coreConfig.accessTokenSecret,
+                signOptions: { expiresIn: coreConfig.accessTokenExpirationTime },
+            }),
         }),
         MongooseModule.forRootAsync({
             useFactory: (coreConfig: CoreConfig) => ({
--- src/core/config/core.config.ts
+++ src/core/config/core.config.ts
@@ -1,50 +1,6 @@
 import { ConfigService } from '@nestjs/config';
-import { configValidationUtility } from '../utils/config-validation.utility';
-import { z } from 'zod';
-import { ZodEnvironments } from '@libs/contracts/enums/enviroments';
 import { Injectable } from '@nestjs/common';
-
-const configSchema = z.object({
-    port: z.string().transform(val => {
-        const numVal = Number(val);
-        if (isNaN(numVal)) {
-            throw new Error('Установите переменную окружения PORT, например: 9000');
-        }
-        return numVal;
-    }),
-    mongoUrl: z.string().nonempty('Установите переменную окружения MONGO_URI, например: mongodb://localhost:27017/my-app-local-db'),
-    env: ZodEnvironments,
-    isSwaggerEnabled: z.string().transform(val => {
-        const booleanValue = configValidationUtility.convertToBoolean(val);
-        if (booleanValue === null) {
-            throw new Error(
-                'Установите переменную окружения IS_SWAGGER_ENABLED для включения/отключения Swagger, например: true, доступные значения: true, false',
-            );
-        }
-        return booleanValue;
-    }),
-    includeTestingModule: z.string().transform(val => {
-        const booleanValue = configValidationUtility.convertToBoolean(val);
-        if (booleanValue === null) {
-            throw new Error(
-                'Установите переменную окружения INCLUDE_TESTING_MODULE для включения/отключения опасного для продакшена TestingModule, например: true, доступные значения: true, false, 0, 1',
-            );
-        }
-        return booleanValue;
-    }),
-    refreshTokenSecret: z.string().nonempty('Установите переменную окружения JWT_REFRESH_SECRET, это опасно для безопасности!'),
-    accessTokenSecret: z.string().nonempty('Установите переменную окружения JWT_ACCESS_SECRET, это опасно для безопасности!'),
-    accessTokenExpirationTime: z
-        .string()
-        .nonempty('Установите переменную окружения JWT_ACCESS_EXPIRATION_TIME, это опасно для безопасности!'),
-    refreshTokenExpirationTime: z
-        .string()
-        .nonempty('Установите переменную окружения JWT_REFRESH_EXPIRATION_TIME, это опасно для безопасности!'),
-    adminUsername: z.string().nonempty('Установите переменную окружения ADMIN_USERNAME, это опасно для безопасности!'),
-    adminPassword: z.string().nonempty('Установите переменную окружения ADMIN_PASS, это опасно для безопасности!'),
-    adminEmail: z.string().nonempty('Установите переменную окружения ADMIN_EMAIL, это опасно для безопасности!'),
-    adminEmailPassword: z.string().nonempty('Установите переменную окружения ADMIN_EMAIL_PASSWORD, это опасно для безопасности!'),
-});
+import { configSchema } from './config.schema';
 
 @Injectable()
 export class CoreConfig {
