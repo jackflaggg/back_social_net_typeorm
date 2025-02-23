77510ba3a0d6d0a2061747d85b289cf60de955b3
jackflaggg <rasul.khamzinnn@gmail.com>
2025-02-23 19:23:41 +0500

    добавил нормалньое удаление!

--- src/features/bloggers-platform/comments/application/usecases/like-comment.usecase.ts
+++ src/features/bloggers-platform/comments/application/usecases/like-comment.usecase.ts
@@ -1,10 +1,6 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { UserPgRepository } from '../../../../user-accounts/infrastructure/postgres/user/user.pg.repository';
 import { CommentsPgRepository } from '../../infrastructure/postgres/comments.pg.repository';
 import { StatusPgRepository } from '../../../likes/infrastructure/postgres/status.pg.repository';
-import { calculateStatus } from '../../../../../core/utils/like/features/calculate.status';
-import { likeViewModel } from '../../../likes/domain/status.entity';
-import { StatusLike } from '../../../../../libs/contracts/enums/status.like';
 
 export class UpdateStatusCommentCommand {
     constructor(
@@ -23,33 +19,15 @@ export class UpdateStatusCommentUseCase implements ICommandHandler<UpdateStatusC
     constructor(
         private readonly commentsRepository: CommentsPgRepository,
         private readonly statusRepository: StatusPgRepository,
-        private readonly usersRepository: UserPgRepository,
     ) {}
     async execute(command: UpdateStatusCommentCommand) {
         const comment = await this.commentsRepository.findCommentById(command.commentId);
 
         const currentStatuses = await this.statusRepository.getStatusComment(comment.id, command.userId);
 
-        let dislike: number = 0;
-        let like: number = 0;
-
-        if (currentStatuses) {
+        if (!currentStatuses) {
             await this.statusRepository.createLikeStatusComment(comment.id, command.userId, command.status);
-
-            const { dislikesCount, likesCount } = calculateStatus(currentStatuses, command.status);
-            dislike = dislikesCount;
-            like = likesCount;
-        } else {
-            const user = await this.usersRepository.findUserById(command.userId!);
-            const dtoStatus: likeViewModel = {
-                userId: user.id.toString(),
-                userLogin: user.login,
-                parentId: comment.id.toString(),
-                status: command.status,
-            };
-
-            like = command.status === StatusLike.enum['Like'] ? 1 : 0;
-            dislike = command.status === StatusLike.enum['Dislike'] ? 1 : 0;
         }
+        await this.statusRepository.updateLikeStatusComment(comment.id, command.userId, command.status);
     }
 }
