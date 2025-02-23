f71a155a510cbb79b8f37ea826c73918b423a7aa
jackflaggg <rasul.khamzinnn@gmail.com>
2025-02-23 15:32:42 +0500

    add comments

--- src/features/testing/api/testing.controller.ts
+++ src/features/testing/api/testing.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { SETTINGS } from '../../../core/settings';
 
-@Controller('testing')
+@Controller(SETTINGS.PATH.TESTING)
 export class TestingController {
     constructor(@InjectDataSource() protected dataSource: DataSource) {}
 
     @HttpCode(HttpStatus.NO_CONTENT)
     @Delete('all-data')
     async deleteAll() {
-        const dataTables = ['users', 'blogs', 'posts', 'likes'];
+        const dataTables = ['users', 'blogs', 'posts', 'comments', 'likes'];
         const query = `TRUNCATE TABLE ${dataTables[0]}, ${dataTables[1]}, ${dataTables[2]}, ${dataTables[3]} RESTART IDENTITY cascade`;
         await this.dataSource.query(query);
         try {
