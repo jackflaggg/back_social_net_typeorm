ee10877f0537d5dabfeb71ba21c0bbab069272da
jackflaggg <rasul.khamzinnn@gmail.com>
2025-03-20 20:28:13 +0500

    add typeorm

--- src/features/bloggers-platform/blogs/application/usecases/delete-blog.usecase.ts
+++ src/features/bloggers-platform/blogs/application/usecases/delete-blog.usecase.ts
@@ -1,6 +1,4 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { BlogsPgRepository } from '../../infrastructure/postgres/blogs.pg.repository';
-import { NotFoundDomainException } from '../../../../../core/exceptions/incubator-exceptions/domain-exceptions';
 import { BlogsRepositoryOrm } from '../../infrastructure/typeorm/blogs.pg.repository';
 
 export class DeleteBlogCommand {
@@ -13,10 +11,7 @@ export class DeleteBlogUseCase implements ICommandHandler<DeleteBlogCommand> {
     async execute(command: DeleteBlogCommand) {
         const blog = await this.blogRepository.findBlogById(command.blogId);
 
-        if (!blog) {
-            throw NotFoundDomainException.create('блог не найден', 'blogId');
-        }
-
-        // await this.blogRepository.deleteBlog(blog);
+        blog.makeDeleted();
+        await this.blogRepository.save(blog);
     }
 }
