6c0559357872238e34dac6294bb0830087c43a97
jackflaggg <rasul.khamzinnn@gmail.com>
2025-03-25 23:50:45 +0500

    new migrations!

--- src/features/bloggers-platform/posts/infrastructure/typeorm/query/posts.pg.query.repository.ts
+++ src/features/bloggers-platform/posts/infrastructure/typeorm/query/posts.pg.query.repository.ts
@@ -7,6 +7,7 @@ import { getPostsQuery } from '../../../utils/post/query.insert.get';
 import { Post } from '../../../domain/typeorm/post.entity';
 import { NotFoundDomainException } from '../../../../../../core/exceptions/incubator-exceptions/domain-exceptions';
 import { Blog } from '../../../../blogs/domain/typeorm/blog.entity';
+import { PaginatedViewDto } from '../../../../../../core/dto/base.paginated.view-dto';
 
 @Injectable()
 export class PostsQueryRepositoryOrm {
@@ -17,19 +18,32 @@ export class PostsQueryRepositoryOrm {
         const updatedSort = sortBy === 'createdAt' ? 'created_at' : sortBy.toLowerCase();
 
         const offset = (pageNumber - 1) * pageSize;
-        const checkBlogId = blogId ? `AND p.'blog_id' = ${blogId}` : ``;
-        const queryBuilderToPosts = await this.postRepositoryOrm
+
+        const checkBlogId = blogId ? `AND p.blog_id = '${blogId}'` : '';
+
+        const subQueryToCountPosts = `(SELECT COUNT(*) FROM posts WHERE deleted_at IS NULL ${blogId ? `AND blog_id = :blogId` : ''}) AS totalCount`;
+
+        const resultPosts = await this.postRepositoryOrm
             .createQueryBuilder('p')
             .select([
                 'p.id AS id, p.title AS title, p.short_description AS shortDescription, p.content AS content, p.created_at AS createdAt, p.blog_id AS blogId, b.name AS blogName',
+                subQueryToCountPosts,
             ])
             .leftJoin(Blog, 'b', 'p.blog_id = b.id')
             .where(`p.deleted_at IS NULL ${checkBlogId}`)
+            .setParameter('blogId', blogId)
             .orderBy(`p.${updatedSort}`, sortDirection)
             .skip(offset)
             .take(pageSize)
             .getRawMany();
-        return queryBuilderToPosts;
+        const totalCount = resultPosts.length > 0 ? Number(resultPosts[0].totalcount) : 0;
+        const postsView = resultPosts.map(post => PostViewDto.mapToView(post));
+        return PaginatedViewDto.mapToView<PostViewDto[]>({
+            items: postsView,
+            page: pageNumber,
+            size: pageSize,
+            totalCount: totalCount,
+        });
     }
 
     async getPost(postId: string, userId: string | null) {
