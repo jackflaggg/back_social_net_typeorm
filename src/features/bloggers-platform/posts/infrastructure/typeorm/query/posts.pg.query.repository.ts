126aeca396bedb32ea3f7da30ccffffd1f8d3b71
jackflaggg <rasul.khamzinnn@gmail.com>
2025-03-28 14:45:10 +0500

    add quiz

--- src/features/bloggers-platform/posts/infrastructure/typeorm/query/posts.pg.query.repository.ts
+++ src/features/bloggers-platform/posts/infrastructure/typeorm/query/posts.pg.query.repository.ts
@@ -9,6 +9,9 @@ import { NotFoundDomainException } from '../../../../../../core/exceptions/incub
 import { Blog } from '../../../../blogs/domain/typeorm/blog.entity';
 import { PaginatedPostViewDto } from '../../../../../../core/dto/base.paginated.view-dto';
 import { PaginationParams } from '../../../../../../core/dto/base.query-params.input-dto';
+import { StatusLike } from '../../../../../../libs/contracts/enums/status/status.like';
+import { CommentsStatus } from '../../../../likes/domain/typeorm/comments/comments.status.entity';
+import { PostStatus } from '../../../../likes/domain/typeorm/posts/post.status.entity';
 
 @Injectable()
 export class PostsQueryRepositoryOrm {
@@ -54,17 +57,46 @@ export class PostsQueryRepositoryOrm {
     }
 
     async getPost(postId: string, userId?: string) {
-        const result = await this.postRepositoryOrm
+        const queryBuilder = this.postRepositoryOrm
             .createQueryBuilder('p')
             .select([
                 'p.id AS id, p.title AS title, p.short_description AS "shortDescription", p.content AS content, p.created_at AS "createdAt", p.blog_id AS "blogId", b.name AS "blogName"',
             ])
             .leftJoin(Blog, 'b', 'b.id = p.blog_id')
-            .where('p.deleted_at IS NULL AND p.id = :postId', { postId })
-            .getRawOne();
+            .where('p.deleted_at IS NULL AND p.id = :postId', { postId });
+
+        if (userId) {
+            queryBuilder.addSelect(subQuery => {
+                return subQuery
+                    .select(`COALESCE(status, '${StatusLike.enum['None']}')`)
+                    .from(PostStatus, 'ps')
+                    .where('c.id = ps.post_id')
+                    .andWhere('ps.user_id = :userId', { userId });
+            }, 'myStatus');
+        } else {
+            queryBuilder.addSelect(`'${StatusLike.enum['None']}'`, 'myStatus');
+        }
+
+        queryBuilder
+            .addSelect(qb => {
+                return qb
+                    .select('COUNT(status)')
+                    .from(PostStatus, 'ps')
+                    .where('ps.status = :likeStatuses', { likeStatuses: StatusLike.enum['Like'] });
+            }, 'likesCount')
+            .addSelect(qb => {
+                return qb
+                    .select('COUNT(status)')
+                    .from(PostStatus, 'ps')
+                    .where('ps.status = :dislikeStatuses', { dislikeStatuses: StatusLike.enum['Dislike'] });
+            }, 'dislikesCount');
+
+        const result = await queryBuilder.getRawOne();
+
         if (!result) {
             throw NotFoundDomainException.create('пост не найден', 'postId');
         }
+
         return PostViewDto.mapToView(result);
     }
 }
