14e7c2a42d9c31bb7c770bfb938d4a68966a8d16
jackflaggg <rasul.khamzinnn@gmail.com>
2025-04-09 17:19:41 +0500

    add index

--- src/features/quiz/questions/infrastructure/typeorm/query/questions.query-repository.ts
+++ src/features/quiz/questions/infrastructure/typeorm/query/questions.query-repository.ts
@@ -3,9 +3,41 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Question } from '../../../domain/question.entity';
 import { Repository } from 'typeorm';
 import { GetQuestionsQueryParams } from '../../../dto/api/get-questions-query-params.dto';
+import { PaginationParams } from '../../../../../../core/dto/base.query-params.input-dto';
 
 @Injectable()
 export class QuestionsQueryRepositoryOrm {
     constructor(@InjectRepository(Question) protected questionRepo: Repository<Question>) {}
-    async getQuestions(query: GetQuestionsQueryParams) {}
+    async getQuestions(queryData: GetQuestionsQueryParams) {
+        const { pageSize, pageNumber, sortBy, sortDirection, bodySearchTerm, publishedStatus } = queryData;
+
+        const queryBuilder = this.questionRepo.createQueryBuilder('question').where('question.deletedAt IS NULL');
+
+        if (bodySearchTerm) {
+            queryBuilder.andWhere('question.body ILIKE :bodySearchTerm', { bodySearchTerm: `%${bodySearchTerm}%` });
+        }
+        if (publishedStatus === PublishedStatus.Published) {
+            queryBuilder.andWhere('question.published = :published', { published: true });
+        }
+        if (publishedStatus === PublishedStatus.NotPublished) {
+            queryBuilder.andWhere('question.published = :published', { published: false });
+        }
+
+        const questions = await queryBuilder
+            .orderBy(`question.${sortBy}`, sortDirection.toUpperCase() as 'ASC' | 'DESC')
+            .skip(PaginationParams.calculateSkip({ pageNumber, pageSize }))
+            .take(pageSize)
+            .getMany();
+
+        const questionsCount = await queryBuilder.getCount();
+
+        const questionsView = questions.map(question => QuestionViewDto.mapToView(question));
+
+        return PaginatedQuestionViewDto.mapToView({
+            items: questionsView,
+            page: pageNumber,
+            size: pageSize,
+            totalCount: questionsCount,
+        });
+    }
 }
