99b8b53ffe6b899786d73ca7d4587b430233cadc
jackflaggg <rasul.khamzinnn@gmail.com>
2025-04-14 20:05:27 +0500

    add index

--- src/features/quiz/game/api/pair-game-quiz.users.controller.ts
+++ src/features/quiz/game/api/pair-game-quiz.users.controller.ts
@@ -1,28 +1,5 @@
-import { Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { SETTINGS } from '../../../../core/settings';
-import { CommandBus } from '@nestjs/cqrs';
-import { ValidateUUIDPipe } from '../../../../core/pipes/validation.input.uuid';
 
 @Controller(SETTINGS.PATH.PAIR_GAME_QUIZ_USERS)
-export class PairGameQuizUsersController {
-    constructor(private readonly commandBus: CommandBus) {}
-
-    @Get()
-    async getQuestions(@Query() query: any) {}
-
-    @HttpCode(HttpStatus.CREATED)
-    @Post()
-    async createQuestion() {}
-
-    @HttpCode(HttpStatus.NO_CONTENT)
-    @Put(':questionId')
-    async updateQuestion(@Param('questionId', ValidateUUIDPipe) questionId: string) {}
-
-    @HttpCode(HttpStatus.NO_CONTENT)
-    @Put(':questionId/publish')
-    async updateQuestionStatusPublish(@Param('questionId', ValidateUUIDPipe) questionId: string) {}
-
-    @HttpCode(HttpStatus.NO_CONTENT)
-    @Delete(':questionId')
-    async deleteQuestion(@Param('questionId', ValidateUUIDPipe) questionId: string) {}
-}
+export class PairGameQuizUsersController {}
