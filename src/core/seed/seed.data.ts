fc2ca22198332d9d289d73a4ae47678deb12afbc
jackflaggg <rasul.khamzinnn@gmail.com>
2025-04-18 23:38:02 +0500

    add seed

--- src/core/seed/seed.data.ts
+++ src/core/seed/seed.data.ts
@@ -1,3 +1,16 @@
 import { DataSource } from 'typeorm';
+import { User } from '../../features/user-accounts/domain/typeorm/user/user.entity';
 
-export const seedData = async (dataSource: DataSource) => {};
+export const seedData = async (dataSource: DataSource) => {
+    const userRepo = dataSource.getRepository(User);
+
+    const usersCount = await userRepo.count();
+    if (!usersCount) {
+        await userRepo.insert([
+            {
+                login: 'bobbbbb',
+                email: 'test11@mail.ru',
+            },
+        ]);
+    }
+};
